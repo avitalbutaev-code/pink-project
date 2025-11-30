ba1ed799be316f7de63164d409767a6349bed9d4
Avital Butaev <avitalbutaev@hilma.tech>
2025-11-30 15:15:36 +0200

    creates tables

--- Server/my-express-app/db/connection.js
+++ Server/my-express-app/db/connection.js
@@ -3,7 +3,7 @@ let con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "z10mz10m",
-  database: " pink_db",
+  database: "pink_db",
 });
 
 module.exports = con;
--- Server/my-express-app/routes/index.js
+++ Server/my-express-app/routes/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const { createTables } = require("../scripts/createTables");
-/* GET home page. */
+
 router.get("/", function (req, res, next) {
   createTables();
   res.render("index", { title: "Express" });
